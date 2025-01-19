9945a6fd912892babd8437f9f6e459adb9f91d4c
ChinZJ <zhenjie.chin88@gmail.com>
2025-01-20 07:42:23 +0800

    Add List Functionality
    Botling currently echoes commands.
    
    List functionality serves as a foundation for tracking tasks.
    
    Let's,
    * Store all inputs by the user except for "list" and "bye".
    * Display all inputs by the user when "list" is given as an input.
    
    Level-2 functionality implemented according to requirements.

--- src/main/java/Botling.java
+++ src/main/java/Botling.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Botling {
     public static void main(String[] args) {
@@ -7,9 +8,12 @@ public class Botling {
         final String MESSAGE_GREET = "\t Hello! I'm Botling\n"
                 + "\t What can I do for you?\n";
         final String MESSAGE_FAREWELL = "\t Bye. Hope to see you again soon!\n";
+        final String MESSAGE_EMPTY_LIST = "\t There are currently no tasks!\n";
+        final String MESSAGE_ADD = "\t added: ";
 
         Scanner reader = new Scanner(System.in);
         boolean isActive = true;
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println(MESSAGE_LINE + MESSAGE_GREET + MESSAGE_LINE);
 
@@ -19,8 +23,19 @@ public class Botling {
             if (input.equals("bye")) {
                 isActive = false;
                 System.out.println(MESSAGE_LINE + MESSAGE_FAREWELL + MESSAGE_LINE);
+            } else if (input.equals("list")) {
+                if (tasks.size() > 0) {
+                    System.out.print(MESSAGE_LINE);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("\t " + (i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println(MESSAGE_LINE);
+                } else {
+                    System.out.println(MESSAGE_LINE + MESSAGE_EMPTY_LIST + MESSAGE_LINE);
+                }
             } else {
-                System.out.println(MESSAGE_LINE + "\t " + input + "\n" + MESSAGE_LINE);
+                tasks.add(input);
+                System.out.println(MESSAGE_LINE + MESSAGE_ADD + input + "\n" + MESSAGE_LINE);
             }
         }
     }
