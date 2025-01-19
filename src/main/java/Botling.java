e6721e988d069f597b1ff433f04321d6c1593dda
ChinZJ <zhenjie.chin88@gmail.com>
2025-01-20 07:19:32 +0800

    Add Echo Functionality
    Botling currently greets and exits.
    
    Echo functionality serves as a foundation for basic user interaction.
    
    The name of the chatbot needs to be personalized for our chatbot.
    
    Let's,
    * Echo commands from user input.
    * Exit when "bye" is given as an input.
    
    Level-1 functionality implemented according to requirements.

--- src/main/java/Botling.java
+++ src/main/java/Botling.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Botling {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
-                " Hello! I'm Botling\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(logo);
+        final String MESSAGE_LINE =
+                "\t____________________________________________________________\n";
+        final String MESSAGE_GREET = "\t Hello! I'm Botling\n"
+                + "\t What can I do for you?\n";
+        final String MESSAGE_FAREWELL = "\t Bye. Hope to see you again soon!\n";
+
+        Scanner reader = new Scanner(System.in);
+        boolean isActive = true;
+
+        System.out.println(MESSAGE_LINE + MESSAGE_GREET + MESSAGE_LINE);
+
+
+        while (isActive) {
+            String input = reader.nextLine();
+            if (input.equals("bye")) {
+                isActive = false;
+                System.out.println(MESSAGE_LINE + MESSAGE_FAREWELL + MESSAGE_LINE);
+            } else {
+                System.out.println(MESSAGE_LINE + "\t " + input + "\n" + MESSAGE_LINE);
+            }
+        }
     }
 }
