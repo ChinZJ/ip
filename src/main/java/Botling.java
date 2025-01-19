116e7c3104fd6de106e63b6380660264fff2b3d2
ChinZJ <zhenjie.chin88@gmail.com>
2025-01-20 06:55:59 +0800

    Renamed Chatbots and Greetings
    The codebase contains Duke as a template placeholder.
    
    The name of the chatbot needs to be personalized for our chatbot.
    
    Let's,
    * Remove all Duke references from the source code.
    * Rename chatbot to Botling.
    * Implement basic greeting and exit functionality.
    
    Level-0 functionality implemented according to requirements.

---
+++ src/main/java/Botling.java
@@ -0,0 +1,11 @@
+public class Botling {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n" +
+                " Hello! I'm Botling\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
