80f7bf97a07c7f72d04581778aeef83fb5418588
ChinZJ <zhenjie.chin88@gmail.com>
2025-01-20 22:50:11 +0800

    Add InvalidInputException.java
    Currently exceptions are all handled under if else control blocks.
    
    Unexpected inputs should be handled by exceptions.
    
    Lets,
    * Create an InvalidInputException class.
    * Create respective methods that would print handled errors.
    
    Level-5 functionality implemented according to requirements.

--- src/main/java/InvalidInputException.java
+++ src/main/java/InvalidInputException.java
@@ -1,7 +1,59 @@
-package PACKAGE_NAME;
+import java.lang.Exception;
 
-public class InvalidInputException extends RuntimeException {
-  public InvalidInputException(String message) {
-    super(message);
-  }
+public class InvalidInputException extends Exception {
+    public InvalidInputException() {
+        super();
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public void unknownCmd() {
+        System.out.println(MsgConstants.MSG_LINE.getString()
+                + MsgConstants.MSG_INVALID_UNKNOWN.getString()
+                + MsgConstants.MSG_LINE.getString());
+    }
+
+    public void unknownList() {
+        System.out.println(MsgConstants.MSG_LINE.getString()
+                + MsgConstants.MSG_INVALID_UNKNOWN.getString()
+                + MsgConstants.MSG_LINE.getString());
+    }
+
+    public void unknownMark(String name, int size) {
+        System.out.println(MsgConstants.MSG_LINE.getString()
+                + MsgConstants.MSG_INVALID_CMD_P1.getString()
+                + name
+                + MsgConstants.MSG_INVALID_CMD_P2.getString()
+                + name + MsgConstants.MSG_INVALID_CMD_MARK.getString() + size + "\n"
+                + MsgConstants.MSG_LINE.getString());
+    }
+
+    public void unknownTodo() {
+        System.out.println(MsgConstants.MSG_LINE.getString()
+                + MsgConstants.MSG_INVALID_CMD_P1.getString()
+                + MsgConstants.CMD_TODO.getString()
+                + MsgConstants.MSG_INVALID_CMD_P2.getString()
+                + MsgConstants.CMD_TODO.getString() + MsgConstants.MSG_INVALID_CMD_TODO.getString()
+                + MsgConstants.MSG_LINE.getString());
+    }
+
+    public void unknownDeadline() {
+        System.out.println(MsgConstants.MSG_LINE.getString()
+                + MsgConstants.MSG_INVALID_CMD_P1.getString()
+                + MsgConstants.CMD_DEADLINE.getString()
+                + MsgConstants.MSG_INVALID_CMD_P2.getString()
+                + MsgConstants.CMD_DEADLINE.getString() + MsgConstants.MSG_INVALID_CMD_DEADLINE.getString()
+                + MsgConstants.MSG_LINE.getString());
+    }
+
+    public void unknownEvent() {
+        System.out.println(MsgConstants.MSG_LINE.getString()
+                + MsgConstants.MSG_INVALID_CMD_P1.getString()
+                + MsgConstants.CMD_EVENT.getString()
+                + MsgConstants.MSG_INVALID_CMD_P2.getString()
+                + MsgConstants.CMD_EVENT.getString() + MsgConstants.MSG_INVALID_CMD_EVENT.getString()
+                + MsgConstants.MSG_LINE.getString());
+    }
 }
