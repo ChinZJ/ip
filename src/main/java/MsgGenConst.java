47ac3eafa97272f67d59a236a0245b42bb231699
ChinZJ <zhenjie.chin88@gmail.com>
2025-01-21 20:46:36 +0800

    Refactor MsgConstants into MsgGenConst and CmdConst
    Current MsgConstants contains all String constants.
    
    However, different class uses different parts of the constants.
    Constants should be unique to the specific class.
    
    Lets,
    * Refactor MsgConstants into MsgGenConst and CmdConst.
    * MsgGenConst will be for messages produced by Botling.
    * CmdConst will be for commands, as well as their expected syntax.

--- src/main/java/MsgGenConst.java
+++ src/main/java/MsgGenConst.java
@@ -1,4 +1,4 @@
-public enum MsgConstants {
+public enum MsgGenConst {
     // For outputs by Botling.
     MSG_LINE("\t____________________________________________________________\n"),
     MSG_GREET("\t Hello! I'm Botling\n"
@@ -15,45 +15,19 @@ public enum MsgConstants {
     MSG_TASK_UNDONE("\t Ok, I've marked this task as not done yet:\n"),
     MSG_TASK_DELETE("\t Noted. I've removed this task: \n"),
 
-    MSG_EMPTY("EMPTY"),
-
     // For unexpected inputs
     MSG_INVALID_CMD_P1("\t OOPS!!! The format of "),
     MSG_INVALID_CMD_P2(" should be "),
-    MSG_INVALID_CMD_MARK(" <X>, where X is a positive integer <= "),
-    MSG_INVALID_CMD_TODO(" <name>.\n"),
-    MSG_INVALID_CMD_DEADLINE(" <name> /by <deadline>.\n"),
-    MSG_INVALID_CMD_EVENT(" <name> /from <start> /to <end>.\n"),
-    MSG_INVALID_UNKNOWN("\t OOPS!!! This command does not exist(yet).\n"),
-
-    // Unique Commands.
-    CMD_BYE("bye"),
-    CMD_LIST("list"),
-    CMD_MARK("mark"),
-    CMD_UNMARK("unmark"),
-    CMD_DELETE("delete"),
-    CMD_TODO("todo"),
-    CMD_DEADLINE("deadline"),
-    CMD_EVENT("event"),
-    CMD_BY(" /by "),
-    CMD_FROM(" /from "),
-    CMD_TO(" /to "),
-
-    // Tasks regex.
-    TASK_MARK("mark -?\\d+"),
-    TASK_UNMARK("unmark -?\\d+"),
-    TASK_DELETE("delete -?\\d+"),
-    TASK_TODO("todo .+"),
-    TASK_DEADLINE("deadline .+ /by .+"),
-    TASK_EVENT("event .+ /from .+ /to .+");
+    MSG_INVALID_UNKNOWN("\t OOPS!!! This command does not exist(yet).\n");
 
     private final String message;
 
-    MsgConstants(String message) {
+    MsgGenConst(String message) {
         this.message = message;
     }
 
     public String getString() {
         return message;
     }
+
 }
