cb81ded2ab8d8bc4f2b28dabda5c96da379b3177
ChinZJ <zhenjie.chin88@gmail.com>
2025-01-27 16:03:31 +0800

    Update JUnit Test

--- src/test/java/botling/TaskListWriterTest.java
+++ src/test/java/botling/TaskListWriterTest.java
@@ -68,15 +68,15 @@ public class TaskListWriterTest {
 
     @Test
     public void filePresentTest() {
-        String expectedMsg = "\t Attempting to retrieve history...\n"
-                + "\t Data folder found!\n"
-                + "\t History file found! Restoring data...\n";
+        String expectedMsg = "Attempting to retrieve history...\n"
+                + "Data folder found!\n"
+                + "History file found! Restoring data...\n";
         String expectedFileString = "todo\n \ntrue\n"
                 + "deadline\ntonight!\n \nfalse\n"
                 + "event\n23 Jan 2025 0000\n24 Jan 2025 2359\n \ntrue\n";
-        String expectedListString = "\t 1. [T][X]  \n"
-                + "\t 2. [D][ ]   (by: tonight!)\n"
-                + "\t 3. [E][X]   (from: 23 Jan 2025 0000 to: 24 Jan 2025 2359) (date)\n";
+        String expectedListString = " 1. [T][X]  \n"
+                + " 2. [D][ ]   (by: tonight!)\n"
+                + " 3. [E][X]   (from: 23 Jan 2025 0000 to: 24 Jan 2025 2359) (date)\n";
 
         ToDo first = new ToDo(" ", true);
         Deadlines second = new Deadlines(" ", "tonight!");
@@ -107,9 +107,9 @@ public class TaskListWriterTest {
         File historyFile = new File("./data/history.txt");
         historyFile.delete();
 
-        String expectedMsg = "\t Attempting to retrieve history...\n"
-                + "\t Data folder found!\n"
-                + "\t No history file found! Creating history file...\n";
+        String expectedMsg = "Attempting to retrieve history...\n"
+                + "Data folder found!\n"
+                + "No history file found! Creating history file...\n";
 
         TaskListWriter tester = new TaskListWriter();
         TaskList testList = new TaskList();
@@ -128,9 +128,9 @@ public class TaskListWriterTest {
         historyFile.delete();
         dataFolder.delete();
 
-        String expectedMsg = "\t Attempting to retrieve history...\n"
-                + "\t No data folder found! Creating data folder...\n"
-                + "\t No history file found! Creating history file...\n";
+        String expectedMsg = "Attempting to retrieve history...\n"
+                + "No data folder found! Creating data folder...\n"
+                + "No history file found! Creating history file...\n";
 
         TaskListWriter tester = new TaskListWriter();
         TaskList testList = new TaskList();
@@ -150,9 +150,9 @@ public class TaskListWriterTest {
             writer.write(fileHistory);
         }
 
-        String expectedMsg = "\t Attempting to retrieve history...\n"
-                + "\t Data folder found!\n"
-                + "\t History file found! Restoring data...\n";
+        String expectedMsg = "Attempting to retrieve history...\n"
+                + "Data folder found!\n"
+                + "History file found! Restoring data...\n";
 
         String input = "y";
         provideInput(input);
